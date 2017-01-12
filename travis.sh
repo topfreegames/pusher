d7c573fc0ceac2f085bba80e05d4ec3565e75bf6
Bernardo Heynemann <heynemann@gmail.com>
2017-01-12 11:45:57 -0200

    Travis keeping both librdkafka versions

--- travis.sh
+++ travis.sh
@@ -2,13 +2,23 @@
 
 set -xe
 
-git clone --depth 1 --branch "$LIBRDKAFKA_VERSION" https://github.com/edenhill/librdkafka.git
-(
-    cd librdkafka
-    ./configure
-    make
-    sudo make install
-)
+LIBRDKAFKA_PATH=$HOME/.cache/librdkafka/$LIBRDKAFKA_VERSION
+
+mkdir -p $HOME/.cache/librdkafka
+
+if [ ! -d $LIBRDKAFKA_PATH ]; then
+    git clone --depth 1 --branch "$LIBRDKAFKA_VERSION" https://github.com/edenhill/librdkafka.git $LIBRDKAFKA_PATH
+    (
+	cd $LIBRDKAFKA_PATH
+	./configure
+	make
+    )
+fi
+cd $LIBRDKAFKA_PATH
+git fetch -a
+git reset --hard
+git pull
+sudo make install
 sudo ldconfig
 
 sleep 3
