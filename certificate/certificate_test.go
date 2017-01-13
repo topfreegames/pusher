9a16c6804c941573f75d3c5017d2dba0490d584f
cscatolini <camila.scatolini@gmail.com>
2017-01-13 10:55:42 -0200

    Certificate are unit testes.

--- certificate/certificate_test.go
+++ certificate/certificate_test.go
@@ -40,89 +40,91 @@ func verifyHostname(cert tls.Certificate) error {
 }
 
 var _ = Describe("Certificate", func() {
-	Describe("PKCS#12", func() {
-		It("Should load valid certificate from P12File", func() {
-			cer, err := FromP12File("../tls/_fixtures/certificate-valid.p12", "")
-			Expect(err).NotTo(HaveOccurred())
-			// Expect(cer.Leaf).NotTo(BeNil())
-			// Expect(cer.Leaf.VerifyHostname("APNS/2 Development IOS Push Services: com.sideshow.Apns2")).To(BeNil())
-			Expect(verifyHostname(cer)).To(BeNil())
+	Describe("[Unit]", func() {
+		Describe("PKCS#12", func() {
+			It("Should load valid certificate from P12File", func() {
+				cer, err := FromP12File("../tls/_fixtures/certificate-valid.p12", "")
+				Expect(err).NotTo(HaveOccurred())
+				// Expect(cer.Leaf).NotTo(BeNil())
+				// Expect(cer.Leaf.VerifyHostname("APNS/2 Development IOS Push Services: com.sideshow.Apns2")).To(BeNil())
+				Expect(verifyHostname(cer)).To(BeNil())
+			})
+
+			It("Should load valid certificate from P12Bytes", func() {
+				bytes, _ := ioutil.ReadFile("../tls/_fixtures/certificate-valid.p12")
+				cer, err := FromP12Bytes(bytes, "")
+				Expect(err).NotTo(HaveOccurred())
+				Expect(verifyHostname(cer)).To(BeNil())
+			})
+
+			It("Should load encrypted certificate from P12File", func() {
+				cer, err := FromP12File("../tls/_fixtures/certificate-valid-encrypted.p12", "password")
+				Expect(err).NotTo(HaveOccurred())
+				Expect(verifyHostname(cer)).To(BeNil())
+			})
+
+			It("Should fail if there is no P12File", func() {
+				cer, err := FromP12File("", "")
+				Expect(err.Error()).To(Equal("open : no such file or directory"))
+				Expect(cer.Certificate).To(BeNil())
+			})
+
+			It("Should fail if bad password is provided", func() {
+				cer, err := FromP12File("../tls/_fixtures/certificate-valid-encrypted.p12", "")
+				Expect(err.Error()).To(Equal("pkcs12: decryption password incorrect"))
+				Expect(cer.Certificate).To(BeNil())
+			})
 		})
 
-		It("Should load valid certificate from P12Bytes", func() {
-			bytes, _ := ioutil.ReadFile("../tls/_fixtures/certificate-valid.p12")
-			cer, err := FromP12Bytes(bytes, "")
-			Expect(err).NotTo(HaveOccurred())
-			Expect(verifyHostname(cer)).To(BeNil())
-		})
+		Describe("PEM", func() {
+			It("Should load valid certificate from PemFile", func() {
+				cer, err := FromPemFile("../tls/_fixtures/certificate-valid.pem", "")
+				Expect(err).NotTo(HaveOccurred())
+				Expect(verifyHostname(cer)).To(BeNil())
+			})
 
-		It("Should load encrypted certificate from P12File", func() {
-			cer, err := FromP12File("../tls/_fixtures/certificate-valid-encrypted.p12", "password")
-			Expect(err).NotTo(HaveOccurred())
-			Expect(verifyHostname(cer)).To(BeNil())
-		})
+			It("Should load valid certificate from PemBytes", func() {
+				bytes, _ := ioutil.ReadFile("../tls/_fixtures/certificate-valid.pem")
+				cer, err := FromPemBytes(bytes, "")
+				Expect(err).NotTo(HaveOccurred())
+				Expect(verifyHostname(cer)).To(BeNil())
+			})
 
-		It("Should fail if there is no P12File", func() {
-			cer, err := FromP12File("", "")
-			Expect(err.Error()).To(Equal("open : no such file or directory"))
-			Expect(cer.Certificate).To(BeNil())
-		})
+			It("Should load encrypted certificate from PemFile", func() {
+				cer, err := FromPemFile("../tls/_fixtures/certificate-valid-encrypted.pem", "password")
+				Expect(err).NotTo(HaveOccurred())
+				Expect(verifyHostname(cer)).To(BeNil())
+			})
 
-		It("Should fail if bad password is provided", func() {
-			cer, err := FromP12File("../tls/_fixtures/certificate-valid-encrypted.p12", "")
-			Expect(err.Error()).To(Equal("pkcs12: decryption password incorrect"))
-			Expect(cer.Certificate).To(BeNil())
-		})
-	})
+			It("Should fail if there is no PemFile", func() {
+				cer, err := FromPemFile("", "")
+				Expect(err.Error()).To(Equal("open : no such file or directory"))
+				Expect(cer.Certificate).To(BeNil())
+			})
 
-	Describe("PEM", func() {
-		It("Should load valid certificate from PemFile", func() {
-			cer, err := FromPemFile("../tls/_fixtures/certificate-valid.pem", "")
-			Expect(err).NotTo(HaveOccurred())
-			Expect(verifyHostname(cer)).To(BeNil())
-		})
+			It("Should fail if bad password is provided", func() {
+				cer, err := FromPemFile("../tls/_fixtures/certificate-valid-encrypted.pem", "badpassword")
+				Expect(err).To(Equal(ErrFailedToDecryptKey))
+				Expect(cer.Certificate).To(BeNil())
+			})
 
-		It("Should load valid certificate from PemBytes", func() {
-			bytes, _ := ioutil.ReadFile("../tls/_fixtures/certificate-valid.pem")
-			cer, err := FromPemBytes(bytes, "")
-			Expect(err).NotTo(HaveOccurred())
-			Expect(verifyHostname(cer)).To(BeNil())
-		})
+			It("Should fail if file with bad key is provided", func() {
+				cer, err := FromPemFile("../tls/_fixtures/certificate-bad-key.pem", "")
+				Expect(err).To(Equal(ErrFailedToParsePKCS1PrivateKey))
+				Expect(cer.Certificate).To(BeNil())
+			})
 
-		It("Should load encrypted certificate from PemFile", func() {
-			cer, err := FromPemFile("../tls/_fixtures/certificate-valid-encrypted.pem", "password")
-			Expect(err).NotTo(HaveOccurred())
-			Expect(verifyHostname(cer)).To(BeNil())
-		})
+			It("Should fail if file with no key is provided", func() {
+				cer, err := FromPemFile("../tls/_fixtures/certificate-no-key.pem", "")
+				Expect(err).To(Equal(ErrNoPrivateKey))
+				Expect(cer.Certificate).To(BeNil())
+			})
 
-		It("Should fail if there is no PemFile", func() {
-			cer, err := FromPemFile("", "")
-			Expect(err.Error()).To(Equal("open : no such file or directory"))
-			Expect(cer.Certificate).To(BeNil())
-		})
-
-		It("Should fail if bad password is provided", func() {
-			cer, err := FromPemFile("../tls/_fixtures/certificate-valid-encrypted.pem", "badpassword")
-			Expect(err).To(Equal(ErrFailedToDecryptKey))
-			Expect(cer.Certificate).To(BeNil())
-		})
-
-		It("Should fail if file with bad key is provided", func() {
-			cer, err := FromPemFile("../tls/_fixtures/certificate-bad-key.pem", "")
-			Expect(err).To(Equal(ErrFailedToParsePKCS1PrivateKey))
-			Expect(cer.Certificate).To(BeNil())
-		})
-
-		It("Should fail if file with no key is provided", func() {
-			cer, err := FromPemFile("../tls/_fixtures/certificate-no-key.pem", "")
-			Expect(err).To(Equal(ErrNoPrivateKey))
-			Expect(cer.Certificate).To(BeNil())
-		})
-
-		It("Should fail if file with no certificate is provided", func() {
-			cer, err := FromPemFile("../tls/_fixtures/certificate-no-certificate.pem", "")
-			Expect(err).To(Equal(ErrNoCertificate))
-			Expect(cer.Certificate).To(BeNil())
+			It("Should fail if file with no certificate is provided", func() {
+				cer, err := FromPemFile("../tls/_fixtures/certificate-no-certificate.pem", "")
+				Expect(err).To(Equal(ErrNoCertificate))
+				Expect(cer.Certificate).To(BeNil())
+			})
 		})
 	})
 })
