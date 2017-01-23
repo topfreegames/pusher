dfbb7749a1737f4c01ecff244f3202d11c2e56d2
Felipe Cavalcanti <fjfcavalcanti@gmail.com>
2017-01-23 16:07:31 -0200

    fix success webhook

--- success_webhook.sh
+++ success_webhook.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 if [ "$LIBRDKAFKA_VERSION" != "master" ]; then
-  VERSION=$(cat ./api/version.go | grep "var VERSION" | awk ' { print $4 } ' | sed s/\"//g)
-  COMMIT=$(git rev-parse --short HEAD)
-
-  curl -X POST "$FARM_URL$TRAVIS_BUILD_NUMBER-v$VERSION-$COMMIT"
+  VERSION=$(cat ./util/version.go | grep "var Version" | awk ' { print $4 } ' | sed s/\"//g)
+  curl -X POST "$FARM_URL$VERSION.$TRAVIS_BUILD_NUMBER"
 fi
