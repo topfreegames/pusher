b295dbaeb8cb1fec8d5a3a5b83801370b3f2a895
Felipe Cavalcanti <fjfcavalcanti@gmail.com>
2017-12-07 11:33:23 -0200

    bump librdkafka and version 🚀

--- debian-install-librdkafka.sh
+++ debian-install-librdkafka.sh
@@ -6,7 +6,7 @@ apt-get install -y gcc g++ python
 
 rm -rf ./librdkafka
 
-git clone --depth 1 --branch "0.9.2.x" https://github.com/edenhill/librdkafka.git
+git clone --depth 1 --branch "v0.11.3" https://github.com/edenhill/librdkafka.git
 (
     cd librdkafka
     ./configure
--- util/version.go
+++ util/version.go
@@ -23,4 +23,4 @@
 package util
 
 //Version is the current version of pusher
-var Version = "3.0.1"
+var Version = "3.1.0"
