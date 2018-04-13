b830753779667c3b95bea46b5a16dca82397e34f
Igor Leao <igor.leao@tfgco.com>
2018-04-13 10:15:19 -0300

    Fix success webhook inner flow

--- success_webhook.sh
+++ success_webhook.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$LIBRDKAFKA_VERSION" != "master" ]; then
+if [ "$TRAVIS_BRANCH" = "master" ] && [ "$LIBRDKAFKA_VERSION" != "master" ]; then
   VERSION=$(cat ./util/version.go | grep "var Version" | awk ' { print $4 } ' | sed s/\"//g)
   curl -X POST "$FARM_URL$VERSION.$TRAVIS_BUILD_NUMBER"
 fi
