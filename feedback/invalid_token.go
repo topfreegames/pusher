76090bb1376d6f5db3fbc0c7e6bca69d342455fd
Moises Silva <moises.silva@tfgco.com>
2019-02-13 19:40:39 -0200

    token delete logic and test

--- feedback/invalid_token.go
+++ feedback/invalid_token.go
@@ -2,6 +2,7 @@ package feedback
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
 	log "github.com/sirupsen/logrus"
@@ -66,7 +67,7 @@ func (i *InvalidTokenHandler) configure(db interfaces.DB) error {
 	flushTime := time.Duration(i.Config.GetInt("invalidToken.flush.time.ms")) * time.Millisecond
 	i.bufferSize = i.Config.GetInt("invalidToken.buffer.size")
 	fmt.Println("flush time:", flushTime)
-	fmt.Println("buffre size:", i.bufferSize)
+	fmt.Println("buffer size:", i.bufferSize)
 
 	i.FlushTicker = time.NewTicker(flushTime)
 	i.Buffer = make([]*InvalidToken, 0, i.bufferSize)
@@ -84,20 +85,30 @@ func (i *InvalidTokenHandler) configure(db interfaces.DB) error {
 
 func (i *InvalidTokenHandler) Start() {
 	i.run = true
+	go i.processMessages()
 }
 
 func (i *InvalidTokenHandler) processMessages() {
+	l := i.Logger.WithFields(log.Fields{
+		"operation": "processMessages",
+	})
+
+	fmt.Println("starting processMessages")
 	for i.run {
 		select {
 		case tk := <-*i.InChan:
+			fmt.Println("MESSAGE IN CHANNEL")
 			i.Buffer = append(i.Buffer, tk)
 
 			if len(i.Buffer) >= i.bufferSize {
+				l.Debug("buffer is full")
 				go i.deleteTokens(i.Buffer)
 				i.Buffer = make([]*InvalidToken, 0, i.bufferSize)
 			}
 
 		case <-i.FlushTicker.C:
+			fmt.Println("FLUSH TIME")
+			l.Debug("flush ticker")
 			go i.deleteTokens(i.Buffer)
 			i.Buffer = make([]*InvalidToken, 0, i.bufferSize)
 
@@ -107,9 +118,62 @@ func (i *InvalidTokenHandler) processMessages() {
 	}
 }
 
+// deleteTokens groups tokens by game and platform and deletes them from the
+// database. A DELETE query is fecthed for each pair <game, platform>. A best
+// effort is applied for each deletion. If there's an error, the next <game,platform>
+// is treated
 func (i *InvalidTokenHandler) deleteTokens(tokens []*InvalidToken) {
-	// l := i.Logger.WithFields(log.Fields{
-	// 	"operation": "deleteTokens",
-	// })
+	m := splitTokens(tokens)
 
+	for platform, games := range m {
+		for game, tks := range games {
+			i.deleteTokensFromGame(tks, game, platform)
+		}
+	}
+}
+
+func splitTokens(tokens []*InvalidToken) map[string]map[string][]string {
+	m := make(map[string]map[string][]string)
+	m[APNSPlatform] = make(map[string][]string)
+	m[GCMPlatform] = make(map[string][]string)
+
+	for _, t := range tokens {
+		m[t.Platform][t.Game] = append(m[t.Platform][t.Game], t.Token)
+	}
+
+	return m
+}
+
+func (i *InvalidTokenHandler) deleteTokensFromGame(tokens []string, game, platform string) error {
+	l := i.Logger.WithFields(log.Fields{
+		"operation": "deleteTokensFromGame",
+		"game":      game,
+		"platform":  platform,
+	})
+
+	// Construct query
+	var queryBuild strings.Builder
+	queryBuild.WriteString(fmt.Sprintf("DELETE FROM %s WHERE token IN (", game+"_"+platform))
+	for j := range tokens {
+		queryBuild.WriteString(fmt.Sprintf("?%d", j))
+		if j == len(tokens)-1 {
+			queryBuild.WriteString(")")
+		} else {
+			queryBuild.WriteString(", ")
+		}
+	}
+
+	queryBuild.WriteString(";")
+	query := queryBuild.String()
+	fmt.Println("QUERY: ", query)
+
+	l.Debug("deleting tokens")
+	_, err := i.Client.DB.Exec(query, []interface{}{tokens}...)
+	if err != nil && err.Error() != "pg: no rows in result set" {
+		// TODO Raven
+		return err
+		l.WithError(err).Error("error deleting tokens")
+	}
+
+	return nil
 }
--- feedback/invalid_token_test.go
+++ feedback/invalid_token_test.go
@@ -1,18 +1,21 @@
 package feedback
 
 import (
+	"time"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/sirupsen/logrus"
 	"github.com/sirupsen/logrus/hooks/test"
 	"github.com/spf13/viper"
 	"github.com/topfreegames/pusher/mocks"
+	"github.com/topfreegames/pusher/testing"
 	"github.com/topfreegames/pusher/util"
 )
 
-var _ = Describe("[Unit]", func() {
+var _ = Describe("InvalidToken Handler", func() {
 	var logger *logrus.Logger
-	// var hook *test.Hook
+	var hook *test.Hook
 	var inChan chan *InvalidToken
 	var config *viper.Viper
 	var err error
@@ -22,7 +25,7 @@ var _ = Describe("[Unit]", func() {
 	configFile := "../config/test.yaml"
 
 	BeforeEach(func() {
-		logger, _ = test.NewNullLogger()
+		logger, hook = test.NewNullLogger()
 
 		config, err = util.NewViperWithConfigFile(configFile)
 		Expect(err).NotTo(HaveOccurred())
@@ -31,12 +34,163 @@ var _ = Describe("[Unit]", func() {
 		inChan = make(chan *InvalidToken, 100)
 	})
 
-	Describe("Creating new InvalidTokenHandler", func() {
-		It("Should return a new handler", func() {
-			handler, err := NewInvalidTokenHandler(logger, config, &inChan, mockClient)
-			Expect(err).NotTo(HaveOccurred())
-			Expect(handler).NotTo(BeNil())
+	Describe("[Unit]", func() {
+		Describe("Creating new InvalidTokenHandler", func() {
+			It("Should return a new handler", func() {
+				handler, err := NewInvalidTokenHandler(logger, config, &inChan, mockClient)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(handler).NotTo(BeNil())
+			})
+		})
+
+		Describe("Flush and Buffer", func() {
+			var tokens []*InvalidToken
+			BeforeEach(func() {
+				tokens = []*InvalidToken{
+					&InvalidToken{
+						Token:    "AAAAAAAAAA",
+						Game:     "boomforce",
+						Platform: "apns",
+					},
+					&InvalidToken{
+						Token:    "BBBBBBBBBB",
+						Game:     "boomforce",
+						Platform: "apns",
+					},
+				}
+				for _, t := range tokens {
+					inChan <- t
+				}
+
+			})
+
+			It("Should flush because buffer is full", func() {
+				config.Set("invalidToken.flush.time.ms", 1000)
+				config.Set("invalidToken.buffer.size", 2)
+
+				logger.Level = logrus.DebugLevel
+				handler, err := NewInvalidTokenHandler(logger, config, &inChan, mockClient)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(handler).NotTo(BeNil())
+
+				handler.Start()
+
+				Eventually(func() []*logrus.Entry { return hook.Entries }).
+					Should(testing.ContainLogMessage("buffer is full"))
+			})
+
+			It("Should flush because reached flush timeout", func() {
+				config.Set("invalidToken.flush.time.ms", 1)
+				config.Set("invalidToken.buffer.size", 200)
+
+				logger.Level = logrus.DebugLevel
+				handler, err := NewInvalidTokenHandler(logger, config, &inChan, mockClient)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(handler).NotTo(BeNil())
+
+				handler.Start()
+
+				Eventually(func() []*logrus.Entry { return hook.Entries }).
+					Should(testing.ContainLogMessage("flush ticker"))
+			})
+		})
+
+		Describe("Deleting from database", func() {
+			var handler *InvalidTokenHandler
+			var err error
+
+			BeforeEach(func() {
+				handler, err = NewInvalidTokenHandler(logger, config, &inChan, mockClient)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(handler).NotTo(BeNil())
+			})
+
+			It("Should create correct queries", func() {
+				handler.Start()
+
+				tokens := []*InvalidToken{
+					&InvalidToken{
+						Token:    "AAAAAAAAAA",
+						Game:     "boomforce",
+						Platform: "apns",
+					},
+					&InvalidToken{
+						Token:    "BBBBBBBBBB",
+						Game:     "boomforce",
+						Platform: "apns",
+					},
+					&InvalidToken{
+						Token:    "CCCCCCCCCC",
+						Game:     "sniper",
+						Platform: "apns",
+					},
+					&InvalidToken{
+						Token:    "DDDDDDDDDD",
+						Game:     "boomforce",
+						Platform: "gcm",
+					},
+					&InvalidToken{
+						Token:    "EEEEEEEEEE",
+						Game:     "sniper",
+						Platform: "gcm",
+					},
+					&InvalidToken{
+						Token:    "FFFFFFFFFF",
+						Game:     "sniper",
+						Platform: "gcm",
+					},
+				}
+
+				for _, t := range tokens {
+					inChan <- t
+				}
+
+				time.Sleep(5 * time.Millisecond)
+				expResults := []struct {
+					Query  string
+					Tokens []string
+				}{
+					{
+						Query:  "DELETE FROM boomforce_apns WHERE token IN (?0, ?1);",
+						Tokens: []string{"AAAAAAAAAA", "BBBBBBBBBB"},
+					},
+					{
+						Query:  "DELETE FROM sniper_apns WHERE token IN (?0);",
+						Tokens: []string{"CCCCCCCCCC"},
+					},
+					{
+						Query:  "DELETE FROM boomforce_gcm WHERE token IN (?0);",
+						Tokens: []string{"DDDDDDDDDD"},
+					},
+					{
+						Query:  "DELETE FROM sniper_gcm WHERE token IN (?0, ?1);",
+						Tokens: []string{"EEEEEEEEEE", "FFFFFFFFFF"},
+					},
+				}
+
+				for _, res := range expResults {
+					Eventually(func() interface{} {
+						for _, exec := range mockClient.Execs[1:] {
+							if exec[0].(string) == res.Query {
+								return exec[1].([]interface{})[0]
+							}
+						}
+						return nil
+					}).Should(Equal(res.Tokens))
+				}
+			})
+
+			// It("should not break if token does not exist in db", func() {
+			// 	mockClient.Error = fmt.Errorf("pg: no rows in result set")
+			// 	handler, err = NewInvalidTokenHandler(logger, config, &inChan, mockClient)
+			// 	Expect(err).NotTo(HaveOccurred())
+			// 	Expect(handler).NotTo(BeNil())
+
+			// })
+
+			// It("should not break if a pg error occured", func() {
+
+			// })
 		})
 	})
-
 })
