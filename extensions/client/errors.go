c7bfd0a28cf8f0e3c87ca478ed0e8bbe021e97c0
Miguel dos Reis <miguel.reis@wildlifestudios.com>
2024-03-22 16:11:55 -0300

    Change error codes

--- extensions/client/errors.go
+++ extensions/client/errors.go
@@ -22,21 +22,21 @@ var (
 func translateError(err error) *pushererrors.PushError {
 	switch {
 	case messaging.IsInvalidArgument(err):
-		return pushererrors.NewPushError("invalid_argument", err.Error())
+		return pushererrors.NewPushError("INVALID_JSON", err.Error())
 	case messaging.IsUnregistered(err):
-		return pushererrors.NewPushError("unregistered_device", err.Error())
+		return pushererrors.NewPushError("DEVICE_UNREGISTERED", err.Error())
 	case messaging.IsSenderIDMismatch(err):
-		return pushererrors.NewPushError("sender_id_mismatch", err.Error())
+		return pushererrors.NewPushError("SENDER_ID_MISMATCH", err.Error())
 	case messaging.IsQuotaExceeded(err):
-		return pushererrors.NewPushError("quota_exceeded", err.Error())
+		return pushererrors.NewPushError("DEVICE_MESSAGE_RATE_EXCEEDED", err.Error())
 	case messaging.IsUnavailable(err):
-		return pushererrors.NewPushError("unavailable", err.Error())
+		return pushererrors.NewPushError("UNAVAILABLE", err.Error())
 	case messaging.IsInternal(err):
-		return pushererrors.NewPushError("firebase_internal_error", err.Error())
+		return pushererrors.NewPushError("INTERNAL_SERVER_ERROR", err.Error())
 	case messaging.IsThirdPartyAuthError(err):
-		return pushererrors.NewPushError("third_party_auth_error", err.Error())
+		return pushererrors.NewPushError("THIRD_PARTY_AUTH_ERROR", err.Error())
 	default:
-		return pushererrors.NewPushError("unknown", err.Error())
+		return pushererrors.NewPushError("UNKNOWN", err.Error())
 	}
 
 	return nil
