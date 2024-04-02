d3b1d0025217bc61d0e48c33332faf68116b0a8c
Miguel dos Reis <miguel.reis@wildlifestudios.com>
2024-04-02 13:24:29 -0300

    Fix test

--- pusher/apns_test.go
+++ pusher/apns_test.go
@@ -104,23 +104,23 @@ var _ = Describe("APNS Pusher", func() {
 				time.Sleep(50 * time.Millisecond)
 			})
 
-			It("should ignore failed handlers", func() {
+			It("should not ignore failed handlers", func() {
 				config.Set("apns.apps", "game,invalidgame")
 				config.Set("apns.certs.invalidgame.authKeyPath", "../tls/authkey_invalid.p8")
 				config.Set("apns.certs.invalidgame.keyID", "oiejowijefiowejf")
 				config.Set("apns.certs.invalidgame.teamID", "aoijeoijfiowejfoij")
 				config.Set("apns.certs.invalidgame.teamID", "com.invalidgame.test")
 
-				pusher, err := NewAPNSPusher(
-					isProduction,
-					config,
-					logger,
-					mockStatsDClient,
-					mockDB,
-					mockPushQueue,
-				)
-				Expect(err).NotTo(HaveOccurred())
-				Expect(pusher.MessageHandler).To(HaveLen(1))
+				Expect(func() {
+					_, _ = NewAPNSPusher(
+						isProduction,
+						config,
+						logger,
+						mockStatsDClient,
+						mockDB,
+						mockPushQueue,
+					)
+				}).To(Panic())
 			})
 		})
 
