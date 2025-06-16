941b5463df6b62503bc3859788acc66d77c0cfbd
alompson <jp.alonso@usp.br>
2025-06-16 09:45:15 -0300

    change log level of dedup message to info

--- extensions/apns/apns_message_handler.go
+++ extensions/apns/apns_message_handler.go
@@ -200,7 +200,7 @@ func (a *APNSMessageHandler) HandleMessages(ctx context.Context, message interfa
 			l.WithFields(log.Fields{
 				"extension": "dedup",
 				"game":      a.appName,
-			}).Debug("duplicate message detected")
+			}).Info("duplicate message detected")
 			extensions.StatsReporterDuplicateMessageDetected(a.StatsReporters, a.appName, "apns")
 			//does not return because we don't want to block the message
 		}
